05e5584584752253705e16af8d59da3eb94127f6
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-12-09 23:18:39 +0900

    [add] type_validater_2_pkg

--- pkg/argumentValidate.go
+++ pkg/argumentValidate.go
@@ -1,14 +1,21 @@
 package pkg
 
+import (
+	"strconv"
+)
+
 /**
  * 引数がint型か判定する
  */
-func ArgumentValidate(args ...interface{}) error {
+func ArgumentValidate(args []string) ([]int, error) {
+	var numbers []int
 	for _, val := range args {
-		if _, ok := val.(int); !ok {
+		num, ok := strconv.Atoi(val)
+		if ok != nil {
 			err := NewError("引数はint型のみ有効です")
-			return err
+			return nil, err
 		}
+		numbers = append(numbers, num)
 	}
-	return nil
+	return numbers, nil
 }
