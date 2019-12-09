4c522c371bbfc2f97af630cf3f43f2e26be13108
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-12-09 21:44:17 +0900

    [add] error_constructor

--- pkg/error.go
+++ pkg/error.go
@@ -4,6 +4,12 @@ type raiseError struct {
 	message string
 }
 
+func NewError(message string) *raiseError {
+	return &raiseError{
+		message,
+	}
+}
+
 func (e *raiseError) Error() string {
 	return e.message
 }
