51b9c2f8d8b43a1c5c5c4d4a24944f7e0125125b
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-12-10 13:43:54 +0900

    [clean] go_lint_with_chg

--- internal/happybirthday/happybirthday_lib.go
+++ internal/happybirthday/happybirthday_lib.go
@@ -1,12 +1,25 @@
-package happyBirthday
+package happybirthday
 
 import (
+	"fmt"
+
 	"github.com/uma-co82/AtCoder-golang/pkg"
 )
 
+func Of(valueOfA, valueOfB int) {
+	err := numberOfCakesValidate(valueOfA, valueOfB)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("Yay!")
+	return
+}
+
 const TOTAL_CAKE_PIECES = 16
 
-func NewNumberOfCakes(valueOfA, valueOfB int) error {
+func numberOfCakesValidate(valueOfA, valueOfB int) error {
 	if valueOfA+valueOfB > TOTAL_CAKE_PIECES {
 		err := pkg.NewError("総数を16以下にして下さい")
 		return err
