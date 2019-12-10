6ecfecf8544c6b3234ec84c860bc2adeb9a86dec
Yuta Isozaki <42496916+uma-co82@users.noreply.github.com>
2019-12-10 23:36:51 +0900

    [add] threeortwo_main_func

---
+++ cmd/threeortwo/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"../../internal/cmd/threeortwo"
+	"github.com/uma-co82/AtCoder-golang/pkg"
+)
+
+func main() {
+	args := os.Args[1:]
+	numbers, err := argumentValidate(args)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	answer, err := threeortwo.Of(numbers[0], numbers[1:])
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Printf("A: %d", answer)
+}
+
+func argumentValidate(args []string) ([]int, error) {
+	var numbers []int
+
+	for _, val := range args {
+		num, ok := strconv.Atoi(val)
+		if ok != nil {
+			err := pkg.NewError("引数は整数値のみ有効です")
+			return nil, err
+		}
+		numbers = append(numbers, num)
+	}
+	return numbers, nil
+}
